db63fd175c509719e420573cecf878b97372bb66
Max <headley.max@gmail.com>
2014-04-04 18:10:42 -0900

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,21 @@
 def full_title(page_title)
 	base_title = "Ruby on Rails Tutorial Sample App"
 	if page_title.empty?
-		base_title
+		base_titleS
 	else
 		"#{base_title} | #{page_title}" 
+	end
+end
+
+def sign_in(user, options={})
+	if options[:no_capybara]
+		remeber_token = User.new_remember_token
+		cookies[:remeber_token] = remember_token
+		user.update_attribute(:remember_token, User.hash(remember_token))
+	else 
+		visit signin_path
+		fill_in "Email", with: user.Email
+		fill_in "Password", with: user.Password
+		click_button "Sign in"
 	end
 end
