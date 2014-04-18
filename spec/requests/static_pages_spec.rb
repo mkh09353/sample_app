56c7f27efa142e2dfc0becfb90cb30e002a0668e
Max <headley.max@gmail.com>
2014-04-17 18:04:19 -0900

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,7 +12,7 @@ describe "Static pages" do
     it { should_not have_title('| Home') }
 
     describe "for signed-in users" do
-      let(:user) {FactoryGirl.create(:user) }
+      let(:user) { FactoryGirl.create(:user) }
       before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
@@ -24,6 +24,17 @@ describe "Static pages" do
         user.feed.each do |item|
           expect(page).to have_selector("li##{item.id}", text: item.content)
         end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path) }
+        it { should have_link("1 followers", href: followers_user_path) }
       end
     end
 	end
